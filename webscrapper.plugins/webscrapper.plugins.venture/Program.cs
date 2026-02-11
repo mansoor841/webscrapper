21bb9abecd17875aac59f489c3f266682642b8e6
Muhammad Mansoor Umer Chauhdry <Mansoor.Umer@innotech-sa.com>
2026-02-11 23:21:17 +0500

    initial login through scrapping successfully

--- webscrapper.plugins/webscrapper.plugins.venture/Program.cs
+++ webscrapper.plugins/webscrapper.plugins.venture/Program.cs
@@ -1,108 +1,48 @@
-﻿using AngleSharp;
-using AngleSharp.Dom;
+﻿using AngleSharp.Dom;
 using AngleSharp.Html.Dom;
 using AngleSharp.Html.Parser;
-using AngleSharp.Io;
-using Flurl;
 using Flurl.Http;
-using System;
 
+var cookieJar = new CookieJar();
 var url = "https://ventureinsga.net/test/root/logon/index.cfm";
-IFlurlClient _httpClient = new FlurlClient();
-var flurlRequest = _httpClient.Request(url);
-var response = await flurlRequest.GetAsync();
-var html = await response.GetStringAsync();
-IBrowsingContext _browsingContext = BrowsingContext.New(Configuration.Default.WithDefaultLoader());
-var document = await _browsingContext.OpenAsync(req => req.Content(html));
-var loginForm = document.QuerySelector<IHtmlFormElement>("form#LogonForm");
-if (loginForm == null)
+var html = await url.GetStringAsync();
+var htmlParser = new HtmlParser();
+var document = await htmlParser.ParseDocumentAsync(html);
+var formData = new Dictionary<string, string>();
+
+foreach (var element in document.QuerySelectorAll<IHtmlInputElement>("input"))
 {
-    throw new InvalidOperationException("Login form not found on the page.");
+    if (!string.IsNullOrEmpty(element.Name))
+    {
+        formData[element.Name] = element.Value ?? "";
+    }
 }
-var formData = new Dictionary<string, string>
-        {
-            { "userloginid", "12821015" },
-            { "userloginname", "ONLINETEST" },
-            { "password", "Am@xit22!" }
-        };
-document = await loginForm.SubmitAsync(formData);
-var link = document.QuerySelector("a[target='MainIS25test']");
-var nextUrl = link.GetAttribute("href");
-var nextContent = await nextUrl.GetStringAsync();
 
-//var url = "https://ventureinsga.net/test/root/logon/index.cfm";
-//var jar = new CookieJar();
-//var html = await url.WithCookies(jar).GetStringAsync();
-//var parser = new HtmlParser();
-//var doc = await parser.ParseDocumentAsync(html);
-//var viewState = doc.QuerySelector<IHtmlInputElement>("input[name=__VIEWSTATE]")?.Value;
-//var formData = new Dictionary<string, string>
-//        {
-//            { "userloginid", "12821015" },
-//            { "userloginname", "ONLINETEST" },
-//            { "password", "Am@xit22!" },
-//            { "__VIEWSTATE", viewState }
-//        };
-//url = "https://ventureinsga.net/test/root/main.cfm";
-//var resultDocument = await url.WithCookies(jar).PostUrlEncodedAsync(formData);
-//html = await resultDocument.GetStringAsync();
-//doc = await parser.ParseDocumentAsync(html);
-//viewState = doc.QuerySelector<IHtmlInputElement>("input[name=__VIEWSTATE]")?.Value;
+formData["userloginid"] = "12821015";
+formData["userloginname"] = "ONLINETEST";
+formData["password"] = "Am@xit22!";
+formData = formData.Where(p => !string.IsNullOrEmpty(p.Key) && !string.IsNullOrEmpty(p.Value)).ToDictionary<string, string>();
 
-////url = "https://ventureinsga.net/test/root/index.cfm";
-////html = await url.WithCookies(jar).GetStringAsync();
+url = "https://ventureinsga.net/test/root/main.cfm";
+var loginResult = await url.WithCookies(out cookieJar).PostUrlEncodedAsync(formData);
+var headerValues = loginResult.Headers.Where(p => p.Name == "Set-Cookie").Select(p => p.Value.Split(";"));
+var queryParams = new Dictionary<string, string>();
+//formData.Add("view", "acct_ReportToolBar");
+//formData.Add("ReportID", "314");
+//formData.Add("ReportStart", "01/01/2000");
+//formData.Add("ReportEnd", "12/31/2025");
+//formData.Add("IsDetailQry", "No");
+//formData.Add("DateSelect", "CustomDates");
+//formData.Add("LinkString", "");
+//formData.Add("ReportType", "CFML");
 
-//Console.WriteLine("test");
+queryParams.Add("view", "reports_cfml");
+queryParams.Add("EUDATA", formData["EUDATA"]);
+queryParams.Add("CFID", "reports_cfml");
+queryParams.Add("CFTOKEN", "reports_cfml");
+queryParams.Add("MarkViewed", "No");
+queryParams.Add("RequestTimeout", "500");
 
 
-
-//try
-//{
-//    var cookieJar = new CookieJar();
-//    string loginUrl = "https://ventureinsga.net/test/root/logon/index.cfm";
-
-//    // 1. GET the initial page to grab ViewState and Cookies
-//    var initialResponse = await loginUrl
-//        .WithCookies(cookieJar)
-//        .GetStringAsync();
-
-//    // 2. Parse with AngleSharp to find the form and its hidden inputs
-//    var context = BrowsingContext.New(Configuration.Default);
-//    var document = await context.OpenAsync(req => req.Content(initialResponse));
-//    //var form = document.QuerySelector<IHtmlFormElement>("form");
-
-//    // Collect ALL inputs from the form (including hidden ViewStates)
-//    var postData = new Dictionary<string, string>();
-//    //foreach (var element in document.QuerySelectorAll<IHtmlInputElement>("input"))
-//    //{
-//    //    if (!string.IsNullOrEmpty(element.Name))
-//    //    {
-//    //        postData[element.Name] = element.Value ?? "";
-//    //    }
-//    //}
-
-//    postData["userloginid"] = "12821015";
-//    postData["userloginname"] = "ONLINETEST";
-//    postData["password"] = "Am@xit22!";
-//    //postData["ctl00$MainContent$LoginControl$LoginButton"] = "Log In";
-
-//    // 4. Submit the form
-//    var postResponse = await loginUrl
-//        .WithCookies(cookieJar)
-//        .PostUrlEncodedAsync(postData)
-//        .ReceiveString();
-
-//    // 5. Now find your anchor tag in the resulting page
-//    var postDoc = await context.OpenAsync(req => req.Content(postResponse));
-//    var anchor = postDoc.QuerySelector<IHtmlAnchorElement>("a[target='MainIS25test']");
-
-//    if (anchor != null)
-//    {
-//        var finalHtml = await anchor.Href.WithCookies(cookieJar).GetStringAsync();
-//        Console.WriteLine("Navigated successfully!");
-//    }
-//}
-//catch(Exception ex)
-//{
-//    Console.WriteLine(ex.ToString()); 
-//}
+html = await url.WithCookies(cookieJar).SetQueryParams(formData).GetStringAsync();
+document = await htmlParser.ParseDocumentAsync(html);

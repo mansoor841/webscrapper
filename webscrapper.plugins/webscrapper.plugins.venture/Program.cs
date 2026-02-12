2cbff154589593255015c88e22346b2f0e36fc18
Muhammad Mansoor Umer Chauhdry <mansoor.umer@innotech-sa.com>
2026-02-12 21:06:46 +0500

    initial web scrapping task done

--- webscrapper.plugins/webscrapper.plugins.venture/Program.cs
+++ webscrapper.plugins/webscrapper.plugins.venture/Program.cs
@@ -2,6 +2,7 @@
 using AngleSharp.Html.Dom;
 using AngleSharp.Html.Parser;
 using Flurl.Http;
+using System.Dynamic;
 
 var cookieJar = new CookieJar();
 var url = "https://ventureinsga.net/test/root/logon/index.cfm";
@@ -22,27 +23,44 @@ formData["userloginid"] = "12821015";
 formData["userloginname"] = "ONLINETEST";
 formData["password"] = "Am@xit22!";
 formData = formData.Where(p => !string.IsNullOrEmpty(p.Key) && !string.IsNullOrEmpty(p.Value)).ToDictionary<string, string>();
-
 url = "https://ventureinsga.net/test/root/main.cfm";
+
 var loginResult = await url.WithCookies(out cookieJar).PostUrlEncodedAsync(formData);
-var headerValues = loginResult.Headers.Where(p => p.Name == "Set-Cookie").Select(p => p.Value.Split(";"));
 var queryParams = new Dictionary<string, string>();
-//formData.Add("view", "acct_ReportToolBar");
-//formData.Add("ReportID", "314");
-//formData.Add("ReportStart", "01/01/2000");
-//formData.Add("ReportEnd", "12/31/2025");
-//formData.Add("IsDetailQry", "No");
-//formData.Add("DateSelect", "CustomDates");
-//formData.Add("LinkString", "");
-//formData.Add("ReportType", "CFML");
 
-queryParams.Add("view", "reports_cfml");
-queryParams.Add("EUDATA", formData["EUDATA"]);
-queryParams.Add("CFID", "reports_cfml");
-queryParams.Add("CFTOKEN", "reports_cfml");
-queryParams.Add("MarkViewed", "No");
-queryParams.Add("RequestTimeout", "500");
+queryParams.Add("view", "reports_CFML");
+queryParams.Add("rpt", "314");
+queryParams.Add("ReportType", "ShowHTML");
+queryParams.Add("ReportStart", "01/01/2026");
+queryParams.Add("ReportEnd", "01/31/2026");
+queryParams.Add("DateSelect", "LastMonth");
 
-
-html = await url.WithCookies(cookieJar).SetQueryParams(formData).GetStringAsync();
+html = await url.WithCookies(cookieJar).SetQueryParams(queryParams).GetStringAsync();
 document = await htmlParser.ParseDocumentAsync(html);
+var pmntTable = document.QuerySelector(".PmntTable")!;
+var list = new List<dynamic>();
+
+foreach (var tr in pmntTable.QuerySelectorAll("tr"))
+{
+    var tds = tr.QuerySelectorAll("td");
+
+    if (tds != null && tds.Length > 0)
+    {
+        dynamic data = new ExpandoObject();
+
+        data.BatchType = tds[0].InnerHtml;
+        data.BatchUserID = tds[1].InnerHtml;
+        data.BatchUserName = tds[2].InnerHtml;
+        data.PaymentDate = tds[3].InnerHtml;
+        data.Policy = tds[4].InnerHtml;
+        data.NamedInsured = tds[5].InnerHtml;
+        data.HowPaid = tds[6].InnerHtml;
+        data.Amount = tds[7].InnerHtml;
+        data.BatchNumber = tds[8].InnerHtml;
+        data.AgentID = tds[9].InnerHtml;
+
+        list.Add(data);
+    }
+}
+
+Console.WriteLine("scrapping complete");

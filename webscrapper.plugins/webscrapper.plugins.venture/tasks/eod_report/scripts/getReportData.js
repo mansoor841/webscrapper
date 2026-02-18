ab598c336ce5c462bbfe1ad996de851c2ae8bb8b
Muhammad Mansoor Umer Chauhdry <mansoor.umer@innotech-sa.com>
2026-02-18 19:59:09 +0500

    AngleSharp & AngleSharp.js packages updated

--- webscrapper.plugins/webscrapper.plugins.venture/tasks/eod_report/scripts/getReportData.js
+++ webscrapper.plugins/webscrapper.plugins.venture/tasks/eod_report/scripts/getReportData.js
@@ -2,7 +2,7 @@ let PmntTable = document.querySelector(".PmntTable");
 let trs = PmntTable.querySelectorAll("tbody tr");
 
 for (var i = 0; i < trs.length; i++) {
-    let tr = trs[i];
+    //let tr = trs[0];
     //let tds = tr.querySelectorAll("td");
 }
 trs.length;

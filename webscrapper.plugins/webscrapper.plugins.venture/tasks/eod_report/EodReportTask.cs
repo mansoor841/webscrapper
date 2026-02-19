d78eb5a3a3509d603fe43f07d3461356337adfe8
Muhammad Mansoor Umer Chauhdry <Mansoor.Umer@innotech-sa.com>
2026-02-19 19:22:57 +0500

    execute javascript change done

--- webscrapper.plugins/webscrapper.plugins.venture/tasks/eod_report/EodReportTask.cs
+++ webscrapper.plugins/webscrapper.plugins.venture/tasks/eod_report/EodReportTask.cs
@@ -1,4 +1,5 @@
 ﻿using AngleSharp.Js;
+using System.Text.Json;
 using webscrapper.plugins.common.classes;
 using webscrapper.plugins.common.interfaces;
 using webscrapper.plugins.venture.classes;
@@ -34,41 +35,8 @@ public class EodReportTask : BaseScrapingTask
         var document = await webScraper.ParseDocumentAsync(html, cancellationToken);
         var jsScriptContent = Utilities.GetJsScript(GetType(), "getReportData.js");
         var result = document.ExecuteScript(jsScriptContent);
-        var obj = JsonConvert.DeserializeObject<PaymentModel>(json);
+        var payments = JsonSerializer.Deserialize<List<PaymentModel>>(result.ToString());
 
-        //var document = await webScraper.ParseHtmlAsync(html, cancellationToken);
-        //var pmntTable = document.QuerySelector(".PmntTable");
-
-        //if (pmntTable == null) throw new InvalidOperationException("Payment Table Not Found");
-
-        //var payments = new List<PaymentModel>();
-        //var columnNames = new[]
-        //{
-        //    "BatchType", "BatchUserID", "BatchUserName", "PaymentDate", "Policy", "NamedInsured", "HowPaid", "Amount", "BatchNumber", "AgentID"
-        //};
-
-        //foreach (var row in pmntTable.QuerySelectorAll("tr").Skip(1))
-        //{
-        //    var rowData = row.GetTableRowData(columnNames);
-
-        //    if (rowData.Count > 0)
-        //    {
-        //        payments.Add(new PaymentModel
-        //        {
-        //            BatchType = rowData.GetValueOrDefault("BatchType"),
-        //            BatchUserID = rowData.GetValueOrDefault("BatchUserID"),
-        //            BatchUserName = rowData.GetValueOrDefault("BatchUserName"),
-        //            PaymentDate = rowData.GetValueOrDefault("PaymentDate"),
-        //            Policy = rowData.GetValueOrDefault("Policy"),
-        //            NamedInsured = rowData.GetValueOrDefault("NamedInsured"),
-        //            HowPaid = rowData.GetValueOrDefault("HowPaid"),
-        //            Amount = rowData.GetValueOrDefault("Amount"),
-        //            BatchNumber = rowData.GetValueOrDefault("BatchNumber"),
-        //            AgentID = rowData.GetValueOrDefault("AgentID")
-        //        });
-        //    }
-        //}
-
-        return null;
+        return payments;
     }
 }
--- webscrapper.plugins/webscrapper.plugins.venture/tasks/eod_report/scripts/getReportData.js
+++ webscrapper.plugins/webscrapper.plugins.venture/tasks/eod_report/scripts/getReportData.js
@@ -8,8 +8,16 @@ for (var i = 0; i < trs.length; i++) {
 
     if (tds.length > 0) {
         resultList.push({
+            BatchType: tds[0].textContent,
+            BatchUserID: tds[1].textContent,
+            BatchUserName: tds[2].textContent,
             PaymentDate: tds[3].textContent,
-            PolicyNo: tds[4].textContent
+            Policy: tds[4].textContent,
+            NamedInsured: tds[5].textContent,
+            HowPaid: tds[6].textContent,
+            Amount: tds[7].textContent,
+            BatchNumber: tds[8].textContent,
+            AgentID: tds[9].textContent
         });
     }
 }
